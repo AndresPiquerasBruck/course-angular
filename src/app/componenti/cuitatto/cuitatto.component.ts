30349e0bab49e500ac0f197cfdb875a35d27642f
Andres Piqueras <andrespiquerasbruck@gmail.com>
2023-12-12 13:40:42 +0100

    Created Cuitatti with Routing

--- src/app/componenti/cuitatto/cuitatto.component.ts
+++ src/app/componenti/cuitatto/cuitatto.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ServizioAutodidactaService } from 'src/app/servizi/servizio-autodidacta.service';
 
 @Component({
   selector: 'app-cuitatto',
   templateUrl: './cuitatto.component.html',
-  styleUrls: ['./cuitatto.component.scss']
+  styleUrls: ['./cuitatto.component.scss'],
 })
 export class CuitattoComponent {
+  cui: any;
+  id: number;
 
+  constructor(
+    private route: ActivatedRoute,
+    private servizioAutodidacta: ServizioAutodidactaService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = +params.get('id')!;
+      this.cui = this.servizioAutodidacta.getCuitatto(this.id);
+    });
+  }
+
+  /* Primer ngOnInit line:  el componente está utilizando el servicio ActivatedRoute 
+  para suscribirse a los cambios 
+  en los parámetros de la URL. Cuando hay cambios en los parámetros de la URL, 
+  la función de devolución de llamada dentro de subscribe se ejecutará. */
 }
--- src/app/servizi/servizio-autodidacta.service.ts
+++ src/app/servizi/servizio-autodidacta.service.ts
@@ -15,8 +15,14 @@ export class ServizioAutodidactaService {
     nome: "Cui posing"},
     {src: 'https://piscespets.com/cdn/shop/articles/267766ab0972edcd5dbd82045a4e5ff2.jpg?v=1623956721',
     nome: "Cui tilted to the left"},
-    {src: 'https://problemparrots.co.uk/wp-content/uploads/Cockatiel.jpg',
+    {src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyDPvzmFc8po0WuacR2dOleMTO1_QcOuIPLw&usqp=CAU',
     nome: "Cui with hat"},
+    {src: 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/122709381/900',
+    nome: "Cui alone against the danger"},
+    {src: 'https://lafeber.com/pet-birds/wp-content/uploads/2018/06/Cockatiel-2.jpg',
+    nome: "Cui nice crest"},
+    {src: "https://petlandkennesaw.com/wp-content/uploads/2022/09/tiels.jpg",
+    nome: "Cui couple"},
   ]
 
   constructor() { }
